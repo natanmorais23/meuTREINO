534718138e2120d279d6340af29e745f18715bf4
Natan Bastos de Morais <natanbastosdemorais7@gmail.com>
2025-02-26 14:55:59 -0300

    fix some bugs

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,5 +1,5 @@
 const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'https://meu-treino-nine.vercel.app/api' 
+  ? 'https://meu-treino-nine.vercel.app' 
   : 'http://localhost:5000';  
 
 export default apiUrl;
